f60865d2ea566a50961ed3cbd0fb00aab76e768b
AndreFGard <andrefsagh@gmail.com>
2023-10-12 07:55:26 -0300

    move /winners and /winner routes to the legacy/ route

--- andrebot-api.js
+++ andrebot-api.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const sqlite3 = require('sqlite3').verbose();
 const bcrypt = require('bcrypt');
-const app = express();
+
+var legacyRouter = require("./routes/legacy");
+
 
 
 // if no env var specifying the database location
@@ -11,9 +13,10 @@ const dbpath = (process.env.DBPATH ? process.env.DBPATH : '/run/user/1001/');
 //const passhash = process.env.PASSWORDHASH;
 const passhash = "$2b$10$kErNPP96nI7VuJzMaINcuOksyn5sHkwlQgv/SMYsYbMA0qDcLbfmG"
 const port = process.env.PORT || 30700
-
 //CREATE TABLE wordlewinners (username varchar(50) NOT NULL, word varchar(20), attempts int, timestamp DATE);
 const addwinner_SQL = `INSERT INTO wordlewinners (username, word, attempts, timestamp) VALUES (?, ?, ?, ?);`
+
+const app = express();
 
 
 let db = new sqlite3.Database(dbpath + 'andrebot.db', sqlite3.OPEN_READWRITE, (err) => {
@@ -22,6 +25,8 @@ let db = new sqlite3.Database(dbpath + 'andrebot.db', sqlite3.OPEN_READWRITE, (e
     }
     console.log('DB CONNECTED')
 });
+
+app.use('/legacy', legacyRouter);
 
 app.get("/", (req, res) =>{
     res.send("hello")
@@ -105,71 +110,6 @@ app.get("/testauthorizationandapikey", (req, res) => {
     
 })
 
-app.post('/winner', (req, res) => {
-    if (!req.headers.authorization) {
-        res.sendStatus(404)
-        return;
-    }
-    if (!isAuthorization(req, res)) {
-        console.log("UNAUTHORIZED ATTEMPT: " + req.ip)
-        res.status(404).send();
-        return;
-    }
-
-
-    console.log(req.body)
-    const winner = req.body;
-    values = [winner.username, winner.word, winner.attempts, winner.timestamp];
-
-    db.run(addwinner_SQL, values, function(err) {
-        if (err) {
-            return res.send("error: " + err);
-        }
-        res.send(`success`);
-        });
-});
-
-
-app.get('/winners', async (req, res) => {
-    if (!req.headers.authorization) {
-        res.sendStatus(404)
-        return;
-    }
-    if (!isAuthorization(req, res)) {
-        console.log("UNAUTHORIZED ATTEMPT: " + req.ip)
-        res.status(404).send();
-        return;
-    }
-
-    
-    const winners = new Map();
-    await new Promise((resolve, reject) => {
-        db.each("SELECT username, COUNT(username) as c FROM wordlewinners GROUP BY username;", (err, row) => {
-            if (err) {
-                reject(err);
-            } else {
-                winners.set(row.username, row.c);
-            }
-        }, (err, count) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(count);
-            }
-        });
-    });
-
-    function logMapElements(value, key, map) {
-        console.log(`m[${key}] = ${value}`);
-    }
-    //DBGwinners.forEach(logMapElements);
-    //DBGconsole.log(winners.size)
-    //DBGconst json = JSON.stringify(Object.fromEntries(winners));
-    //DBGconsole.log(json);
-
-
-    res.json(Object.fromEntries(winners));
-});
 
 app.post('/winnerdeta', (req, res) => {
     if (!req.headers.authorization) {
--- routes/legacy.js
+++ routes/legacy.js
@@ -6,4 +6,71 @@ router.get('/', function(req, res, next) {
   res.send('Legacy route');
 });
 
+router.post('/winner', (req, res, next) => {
+  if (!req.headers.authorization) {
+      res.sendStatus(404)
+      next();
+  }
+  if (!isAuthorization(req, res, next)) {
+      console.log("UNAUTHORIZED ATTEMPT: " + req.ip)
+      res.status(404).send();
+      next();
+  }
+
+
+  console.log(req.body)
+  const winner = req.body;
+  values = [winner.username, winner.word, winner.attempts, winner.timestamp];
+
+  db.run(addwinner_SQL, values, function(err) {
+      if (err) {
+          return res.send("error: " + err);
+      }
+      res.send(`success`);
+      });
+});
+
+
+router.get('/winners', async (req, res, next) => {
+  if (!req.headers.authorization) {
+      res.sendStatus(404)
+      next();
+  }
+  if (!isAuthorization(req, res, next)) {
+      console.log("UNAUTHORIZED ATTEMPT: " + req.ip)
+      res.status(404).send();
+      next();
+  }
+
+  
+  const winners = new Map();
+  await new Promise((resolve, reject) => {
+      db.each("SELECT username, COUNT(username) as c FROM wordlewinners GROUP BY username;", (err, row) => {
+          if (err) {
+              reject(err);
+          } else {
+              winners.set(row.username, row.c);
+          }
+      }, (err, count) => {
+          if (err) {
+              reject(err);
+          } else {
+              resolve(count);
+          }
+      });
+  });
+
+  function logMapElements(value, key, map) {
+      console.log(`m[${key}] = ${value}`);
+  }
+  //DBGwinners.forEach(logMapElements);
+  //DBGconsole.log(winners.size)
+  //DBGconst json = JSON.stringify(Object.fromEntries(winners));
+  //DBGconsole.log(json);
+
+
+  res.json(Object.fromEntries(winners));
+});
+
+
 module.exports = router;
