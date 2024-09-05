6b9413a31f5ff0c50730df8b511c5d9eefeb62b7
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2024-09-05 09:45:43 -0300

    remove old files completely

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET,POST,UPDATE on index */
-router.get('/', function(req, res, next) {
-  res.send('The api is working!');
-});
-
-module.exports = router;
