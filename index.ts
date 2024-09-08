849dd3b716c0d6cfe2c9aee52920efd7dba8067b
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2024-09-08 20:44:04 -0300

    [v1.0] add base index file

---
+++ index.ts
@@ -0,0 +1,28 @@
+//
+import express, {Express, NextFunction, Request, Response} from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json()); //middleware that partes the request's json
+
+
+
+app.get("/", (req: Request, res: Response) => {
+    console.log(req.body);
+    res.send("eae");
+});
+
+import andrebotRoutes from "./routes/andrebot";
+app.use("/andrebot", andrebotRoutes);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).send("not found :)");
+});
+
+app.listen(port, () => {
+    console.log(`starting server at http://localhost:${port}`)
+})
