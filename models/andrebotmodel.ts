7b17fa392baec1384ef4a95b038827518a887c92
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2024-09-11 10:15:37 -0300

    [v1.0] update wins after addwinners request

--- models/andrebotmodel.ts
+++ models/andrebotmodel.ts
@@ -65,9 +65,11 @@ export class AndrebotModel {
     async addUser(username: string, platform: string, wins?: number){
         await this.check_connection();
 
-        let q =  format("INSERT INTO users (username, platform, anon_username, wins, registration) SELECT %L, %L, %L, %s, NOW() WHERE NOT EXISTS " +
+        let q =  format("INSERT INTO users (username, platform, anon_username, wins, registration) " +
+                "SELECT %L, %L, %L, %s, NOW() WHERE NOT EXISTS " +
                 "(SELECT 1 FROM users WHERE username = %L AND platform = %L);",
-                username, platform, this.createAnonUsername(username, platform), Number(wins), username, platform);
+                username, platform, this.createAnonUsername(username, platform),
+                 Number(wins), username, platform);
         
         this.client.query(q)
             .catch((err: Error) => {
@@ -78,24 +80,19 @@ export class AndrebotModel {
     async increment_wins(username: string, platform: string, amount: number){
         await this.check_connection();
         this.client.query(format(
-            'update users set wins = wins+1 where name = %L and platform = %L',
+            'update users set wins = wins+1 where username = %L and platform = %L',
         username, platform));
     }
 
     async addWinner(username: string, loser_username: string, word: string, platform: string, attempts: number){
         await this.check_connection();
         var q1 = "insert into victories (user_id, loser_id, word, platform, attempts, event_date) " +
-                    "values ((SELECT id from users where username = %L and platform = %L), (SELECT id FROM users WHERE username = %L and platform = %L), %L, %L, %s, NOW())";
+                    "values ((SELECT id from users where username = %L and platform = %L), " + 
+                    "(SELECT id FROM users WHERE username = %L and platform = %L), %L, %L, %s, NOW())";
         q1 = format(q1, username, platform, loser_username, platform, word,platform, attempts );
 
         //TODO: deal with error
-        this.client.query(q1)
-            .then( () => {
-                this.increment_wins(username, platform, 1).catch(err => {
-                    console.log(err);
-                });
-                
-            })
+        (this.client.query(q1)
             .catch(async (err:Error) => {
                 if (err.message.includes("null value in column")){
                     console.log('non registered user, trying to add the loser and the winner');
@@ -103,6 +100,11 @@ export class AndrebotModel {
                     await this.addUser(loser_username, platform, 0);
 
                 } else console.log('unknown error: '  + err.message);
+            }))
+            .then( () => {
+                this.increment_wins(username, platform, 1).catch(err => {
+                    console.log(err);
+                }); 
             }).catch(_ => {
                 console.log('failed to register users');
                console.log(_);
