5dfdf6ec680e6b83e021759fb438e9826c229b70
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2024-09-11 12:11:55 -0300

    [v1.0] add authentication

--- routes/andrebot.ts
+++ routes/andrebot.ts
@@ -1,8 +1,10 @@
 import express, {Express, NextFunction, Request, Response} from "express";
 const router = express.Router();
-import { testauth, addWinners, addUser, getRank} from "../controllers/andrebotController";
+import { testauth, addWinners, addUser, getRank, auth} from "../controllers/andrebotController";
 
 router.get('/testauth', testauth );
+
+router.use(auth);
 router.post('/addwinners', addWinners);
 router.post('/adduser', addUser);
 router.get('/getrank', getRank);
