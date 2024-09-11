56057653a8fa3867661cd7c67f02e70d2cf6bb85
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2024-09-11 11:34:26 -0300

    [v1.0] remove  hard coded consts from controllers

--- controllers/andrebotController.ts
+++ controllers/andrebotController.ts
@@ -4,26 +4,27 @@ import {andrebotServices} from "../services/andrebotServices";
 const andrebotService = new andrebotServices();
 
 export const  testauth = async (req: Request, res: Response, next: NextFunction) => {
-    
-    if (!await andrebotService.auth("dsc", "eae")) {
-        res.send("unauthed");
-    }
-    else res.send("ok");
+    if ( !(req.body.platform || req.body.password) ||
+        !await andrebotService.auth(req.body.platform, req.body.password)) {
+            res.status(403).send("unauthed");
+        }
+
+    else res.status(200).send("ok");
 };
 
 export const addWinners = async (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.body.winners);
     andrebotService.addWinners(req.body.winners)
 
-    
     res.status(200).send();
 }
 
 export const addUser = async(req: Request, res: Response, next: NextFunction) => {
     andrebotService.addUser(req.body.user).then(() => {
-        res.status(200);
+        res.status(200).send();
         console.log('ok');
+
     }).catch(_ => {
-        res.status(500);
+        console.log(_);
+        res.status(500).send();
     });
 }
