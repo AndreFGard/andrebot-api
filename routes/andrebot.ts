5610da66f2746ab359129bf8674394673fc70b1b
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2024-09-12 11:24:21 -0300

    [v1.0] remove authorization requirement for getRank

--- routes/andrebot.ts
+++ routes/andrebot.ts
@@ -3,9 +3,10 @@ const router = express.Router();
 import { testauth, addWinners, addUser, getRank, auth} from "../controllers/andrebotController";
 
 router.get('/testauth', testauth );
+router.get('/getrank', getRank);
 
 router.use(auth);
 router.post('/addwinners', addWinners);
 router.post('/adduser', addUser);
-router.get('/getrank', getRank);
+
 export default router;
