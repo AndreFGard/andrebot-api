e68cbef765d51f08c6a4490702ca3a626b47272d
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2024-11-09 17:39:01 -0300

    [v1.3] add cors support

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
 //
 import express, {Express, NextFunction, Request, Response} from "express";
 import dotenv from "dotenv";
-
+import cors from "cors"
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
-
+app.use(cors())
 app.use(express.json()); //middleware that partes the request's json
 app.set('view engine', 'ejs');
-
+app.use(cors())
 
 app.get("/", (req: Request, res: Response) => {
     res.send("eae");
