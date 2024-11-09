690cf49ca679684079a3d4d4b6e5840b51f4e1bc
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2024-11-09 19:04:50 -0300

    add https support

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
 import path from "path";
+import http from "http"
+import https from "https"
+import fs from "fs";
 import express, {Express, NextFunction, Request, Response} from "express";
 import dotenv from "dotenv";
 import cors from "cors"
@@ -17,7 +20,7 @@ app.get("/", (req: Request, res: Response) => {
 
 
 
-app.use('/.well-known', express.static(path.join(__dirname, '.well-known'), { dotfiles: 'allow' }));
+
 
 import andrebotRoutes from "./routes/andrebot";
 app.use("/andrebot", andrebotRoutes);
@@ -26,6 +29,33 @@ app.use((req: Request, res: Response, next: NextFunction) => {
     res.status(404).send("not found :)");
 });
 
-app.listen(port, () => {
-    console.log(`starting server at http://localhost:${port}`)
-})
+
+//if http is enabled
+
+if (Number(port) == 443){
+    const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
+    const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
+    const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+    
+    const credentials = {
+        key: privateKey,
+        cert: certificate,
+        ca: ca
+    };
+    
+    const httpServer = http.createServer(app);
+    const httpsServer = https.createServer(credentials, app);
+
+    httpServer.listen(80, () => {
+        console.log('HTTP Server running on port 80');
+    });
+
+    httpsServer.listen(443, () => {
+        console.log('HTTPS Server running on port 443');
+    });
+}
+else {
+    app.listen(port, () => {
+        console.log(`starting server at http://localhost:${port}`)
+     })
+}
