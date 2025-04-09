fc807e4434396f538414f71e7cfd841fc2cf11ec
Andrefgard <96950645+AndreFGard@users.noreply.github.com>
2025-04-09 17:13:52 -0300

    fix: make it top aligned

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,15 @@ import './App.css'
 import TimetableEditor from './components/tiimetableeditor'
 import { ThemeProvider } from "@/components/theme-provider"
 function App() {
-
   return (
-    <div className="App">
-      <main className=''>
-      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-        <TimetableEditor></TimetableEditor>
-      </ThemeProvider>
+    <div className="w-full">
+      <main className="w-full">
+        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+          <TimetableEditor></TimetableEditor>
+        </ThemeProvider>
       </main>
     </div>
   )
 }
 
-export default App;
+export default App
